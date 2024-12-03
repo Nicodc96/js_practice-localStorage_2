270dc5d6c11a87278bb510e51e4e434f0119a7b2
Nicolas Personal Desktop <diaznicox96@gmail.com>
2024-12-03 19:07:29 -0300

    se agregan comentarios explicativos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-const carritoDeCompras = JSON.parse(localStorage.getItem("lista-compras")) || [];
-// import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11.14.4/src/sweetalert2.js';
+const carritoDeCompras = JSON.parse(localStorage.getItem("lista-compras")) || []; // Refencia a la lista del localStorage
 
+/* 
+    Esto es sólo para la consola, y para tener una guía.
+    Cuando se tenga que entregar, se debe eliminar.
+*/
 if (carritoDeCompras.length > 0){
     console.log("Lista de productos:");
     carritoDeCompras.forEach((producto) => {
@@ -10,6 +13,14 @@ if (carritoDeCompras.length > 0){
     console.log("¡Lista de compras vacío!");
 }
 
+// -----------------------------------------------------
+
+/* 
+    Creo un objeto que tenga atributos nombre, precio, un string que guarda
+    el nombre de la imagen local para usar como 'src' en <img> más tarde.
+    Además le agrego un atributo cantidad = 1 para aclarar que se agrega un
+    producto del seleccionado.
+*/
 const crearObjetoProducto = (nombre, precio, strImagen) => new Object({
     nombre: nombre,
     precio: precio,
@@ -17,15 +28,51 @@ const crearObjetoProducto = (nombre, precio, strImagen) => new Object({
     cantidad: 1
 });
 
-
 window.addEventListener("click", (e) => {
+    // El e.target.matches() sirve para encontrar el primer elemento del DOM que tenga la class="btn-agregar-producto"
     if (e.target.matches(".btn-agregar-producto")){
+        /*
+            Lo que guardo en cardActual es una referencia.
+            Básicamente cuando click en el botón 'Agregar' de
+            alguna de las cards, me guardo la referencia del
+            <div class="card"> en cual hice click.
+
+            Es libre de investigar más acerca de parentNode
+        */
         const cardActual = e.target.parentNode.parentNode;
+        /*
+            Esto es la referencia al <div class="added">
+            que tiene un SVG y un texto que le indica al
+            usuario que se agregó un producto de ese
+            tipo al carrito
+        */
         const divAgregado = e.target.nextElementSibling;
+        /*
+            Esto es lo que simula el efecto de aparecer y
+            desaparecer el "¡Agregado!" al agregar un
+            producto del tipo seleccionado
+
+            Es libre de investigar más acerca de setTimeOut
+        */
         divAgregado.style.opacity = 1;
         setTimeout(() => {
             divAgregado.style.opacity = 0;
         }, 2000);
+
+        /*
+            Primero verifico si la lista de compras del localStorage no
+            está vacío. Si está vacío o no existe un producto del tipo
+            que se quiere agregar en la lista, hago un push a la lista
+            del carrito de compras (de la línea 1) utilizando una
+            función que desarrollé llamado crearObjetoProducto() (línea 24)
+
+            Si existe en la lista (lo verifico por el nombre), no vuelvo a
+            agregar un elemento igual con cantidad = 1. En su lugar, busco el
+            elemento en la lista, y le sumo +1 a su atributo propiedad.
+
+            Finalmente uso una bandera (flag) para indicar que existe ese
+            producto en la lista y no se debe agregar uno nuevo luego.
+        */
         let existente = false;
         if (carritoDeCompras.length > 0){
             carritoDeCompras.forEach((producto) => {
@@ -42,6 +89,7 @@ window.addEventListener("click", (e) => {
             ));
         }    
         
+        // Finalmente, guardo en el localStorage los cambios realizados
         localStorage.setItem("lista-compras", JSON.stringify(carritoDeCompras));
         
         // Nombre del producto
@@ -53,6 +101,7 @@ window.addEventListener("click", (e) => {
     }
 });
 
+// Acá redirijo a la página del carrtito de compras al hacer click en el botón 'Ir al carrito'
 document.querySelector("#btnCarrito").addEventListener("click", () => {
     window.location.href = "./compra.html";
 })
--- js/buy.js
+++ js/buy.js
@@ -1,13 +1,26 @@
-const carritoDeCompras = JSON.parse(localStorage.getItem("lista-compras")) || [];
-const cardBody = document.querySelector(".card-body");
-const cardFooter = document.querySelector(".card-footer");
-const btnComprar = document.querySelector(".btn-agregar-producto");
-const btnLimpiarCarrito = document.querySelector(".btn-limpiar-carrito");
-const btnVolver = document.querySelector("#btnVolver");
+const carritoDeCompras = JSON.parse(localStorage.getItem("lista-compras")) || []; // Lista del localStorage
+const cardBody = document.querySelector(".card-body"); // Referencia al div class="card-body"
+const cardFooter = document.querySelector(".card-footer"); // Referencia al div class="card-footer"
+const btnComprar = document.querySelector(".btn-agregar-producto"); // Referencia al boton 'Comprar'
+const btnLimpiarCarrito = document.querySelector(".btn-limpiar-carrito"); // Referencia al boton 'Limpiar carrito'
+const btnVolver = document.querySelector("#btnVolver"); // Referencia al botón 'Volver a productos'
 
 const divProducto = (producto) => {
     /* Creo los divs contenedores y los uno según correspondan */
     const divContenedor = document.createElement("div");
+    /*
+        Lo que hago acá al momento de agregar un estilo CSS
+        al elemento del DOM que acabo de crear es utilizar,
+        en primer lugar, la función add() del array del tipo
+        DOMTokenList (investigar más si se desea) para agregar
+        estilos al escribirlos como string.
+        Luego, utilizo el 'spread operator' (investigar más si
+        se desea) para desarmar un array de strings donde cada
+        elemento del array es 1 estilo diferente.
+        
+        Esto hace que mi div quede así:
+        <div class="producto mt-3"></div>
+    */
     divContenedor.classList.add(...["producto", "mt-3"]);
     
     const divProductoDescripcion = document.createElement("div");
@@ -30,6 +43,7 @@ const divProducto = (producto) => {
 
     const spanPrecioProducto = document.createElement("span");
     spanPrecioProducto.classList.add("text-green-700");
+    // Acá multiplico precio del producto * cantidad que se hayan solicitado
     spanPrecioProducto.textContent = `$${producto.precio*producto.cantidad}`;
 
     pPrecioProducto.appendChild(spanPrecioProducto);
@@ -39,16 +53,18 @@ const divProducto = (producto) => {
 
     /* Creo el elemento 'img' que contendrá la imagen de referencia de los productos agregados */
     const imgProducto = document.createElement("img");
+    // Acá utilizo el nombre de la imagen que guardé en mi objeto producto
     imgProducto.src = `./img${producto.imgSrc}`;
     imgProducto.classList.add("tiny-image");
-    imgProducto.alt = producto.nombre;
+    imgProducto.alt = producto.nombre; // Agrego un 'alt' (importante para las <img>)
 
     divProductoImagen.appendChild(imgProducto);
 
-    /* Finalmente devuelvo el contenedor con todo el div armado
-
+    /* 
+        Finalmente devuelvo el contenedor con todo el div armado.
         Para una vista a modo de ejemplo de lo que
-        se uniría al html sería algo como esto:
+        se uniría al <div class='card-body'> sería algo como esto:
+
         <div class="producto">
             <div class="producto-descripcion">
                 <p class="font-semibold">(x1) Mate rústico c/ bombilla</p>
@@ -58,10 +74,18 @@ const divProducto = (producto) => {
                 <img src="./img/mate-rustico.webp" alt="productoEjemplo" class="tiny-image">
             </div>
         </div>
+
+        Por cada producto distinto que se haya agregado al localStorage
     */
     return divContenedor;
 }
 
+/*
+    Acá simplemente creo un elemento 'p' para
+    calcular el precio total de todos los
+    productos y devolver algo como:
+    <p class="font-semibold">Total: $5000</p>
+*/
 const pPrecioTotalProductos = (lista) => {
     const pTotal = document.createElement("p");
     pTotal.classList.add("font-semibold");
@@ -74,6 +98,11 @@ const pPrecioTotalProductos = (lista) => {
     return pTotal;
 }
 
+/*
+    En caso de que no existan productos en el array
+    del localStorage, lo que hago es crear un 'p'
+    que mostrará el mensaje '¡Carrito vacío!'.
+*/
 const sinProductos = () => {
     const textNoProductos = document.createElement("p");
     textNoProductos.textContent = "¡Carrito vacío!";
@@ -81,28 +110,52 @@ const sinProductos = () => {
     return textNoProductos;
 }
 
+/*
+    Acá muestro, en compra.html, los productos
+    y sus respectivas cantidades y precios en el
+    <div class="card-body">. Cada producto distinto
+    será un nuevo <div class="producto"> y se unirá
+    uno abajo del otro.
+
+    Utilizo funciones para realizar esta lógica, que
+    no es más que delegación de tareas y para que se
+    vea más claramente el proceso. Cada appendChild()
+    recibe un Element.
+
+    Finalmente y a modo de detalle, utilizo la class de CSS
+    de Tailwind '.hidden' para ocultar los botones de
+    'Comprar' y 'Limpiar carrito' cuando este se encuentre
+    vacío.
+*/
 if (carritoDeCompras.length > 0){
-    console.log("Lista de productos:");
     carritoDeCompras.forEach((producto) => {
-        console.log(`Producto: ${producto.nombre}\nPrecio: $${producto.precio}\nImgUrl: ${producto.imgSrc}\nCantidad: ${producto.cantidad}`);
         cardBody.appendChild(divProducto(producto));
     });
     cardFooter.appendChild(pPrecioTotalProductos(carritoDeCompras));
 } else{
-    console.log("¡Lista de compras vacío!");
     cardBody.appendChild(sinProductos());
     btnComprar.classList.add("hidden");
     btnLimpiarCarrito.classList.add("hidden");
 }
 
+// Redirección al index
 btnVolver.addEventListener("click", () => {
     window.location.href = "./index.html";
 });
 
+/*
+    Al hacer click en el botón 'Limpiar Carrito'
+    lo que hago es agarrar mi lista de productos
+    del localStorage y setar su tamaño (lenght)
+    a 0. Esto, en JS, es una forma de vacíar el
+    array (¡MAGIA!).
+    Finalmente guardo la nueva lista vacía en el
+    localStorage dando a entender que se limpió.
+*/
 btnLimpiarCarrito.addEventListener("click", () => {
     /*
         ¡RECOMIENDO PREGUNTAR ANTES DE ELIMINAR!
-        
+
         CON UN CONFIRM O LO QUE SE PREFIERA
     */
     carritoDeCompras.length = 0;
@@ -111,6 +164,11 @@ btnLimpiarCarrito.addEventListener("click", () => {
     window.location.href = "./compra.html";
 });
 
+/*
+    Botón de comprar, utilizar para redigirse a
+    otra página o sección que contendrá el formulario
+    a completar y para enviar a FormSpree.
+*/
 btnComprar.addEventListener("click", () =>{
     alert("Aca redigir a la página con la información de pago (formulario para enviar a FormSpree)");
 })
